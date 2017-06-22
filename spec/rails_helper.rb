2cfdf5a013f4c046b832222b7b8a27f5b439f883
zulkarnay <bedelbaev@gmail.com>
2017-06-22 09:42:11 +0600

    Setup devise and rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,43 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'devise'
+require 'factory_girl_rails'
+require 'shoulda/matchers'
+require 'support/database_cleaner'
+
+include Warden::Test::Helpers
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(
+    app,
+    phantomjs_logger: Rails.root.join('log', 'poltergeist.log'),
+    inspector: true
+  )
+end
+
+Capybara.javascript_driver = :poltergeist
+
+Capybara.server_port = 3001
+Capybara.app_host = 'http://localhost:3001'
+
+RSpec.configure do |config|
+  #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include Capybara::DSL
+  config.include FactoryGirl::Syntax::Methods
+  config.include Rails.application.routes.url_helpers
+  config.include Warden::Test::Helpers
+  config.include Devise::TestHelpers, type: :controller
+
+end
