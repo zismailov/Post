96a5efb123645a3b80a6689425e900e59202c8f0
zulkarnay <bedelbaev@gmail.com>
2017-06-23 16:11:04 +0600

    Add basic tests for recipients and non-recipient

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -2,16 +2,58 @@ feature 'Messages', js: true do
   let!(:sender) { create(:user) }
   let!(:first_receiver) { create(:user) }
   let!(:second_receiver) { create(:user) }
-
-  before(:each) do
-    login_as(sender, scope: :user)
-    visit root_path
-  end
+  let!(:non_receiver) { create(:user) }
 
   feature 'creating' do
+    before(:each) do
+      login_as(sender, scope: :user)
+      visit root_path
+    end
+
     scenario 'shows successfull flash message' do
       fill_and_send_form
       expect(page).to have_content('Message sent!')
+    end
+  end
+
+  feature 'viewing' do
+    it 'is available for first recipient' do
+      sign_in sender
+      visit root_path
+      fill_and_send_form
+      sign_out sender
+
+      sign_in first_receiver
+      visit root_path
+
+      expect(page).to have_content('Message title')
+      expect(page).to have_content('Lorem Ipsum')
+    end
+
+    it 'is available for second recipient' do
+      sign_in sender
+      visit root_path
+      fill_and_send_form
+      sign_out sender
+
+      sign_in second_receiver
+      visit root_path
+
+      expect(page).to have_content('Message title')
+      expect(page).to have_content('Lorem Ipsum')
+    end
+
+    it 'is not available for non_receiver' do
+      sign_in sender
+      visit root_path
+      fill_and_send_form
+      sign_out sender
+
+      sign_in non_receiver
+      visit root_path
+
+      expect(page).to_not have_content('Message title')
+      expect(page).to_not have_content('Lorem Ipsum')
     end
   end
 
@@ -22,6 +64,7 @@ feature 'Messages', js: true do
     fill_in 'message_title', with: 'Message title'
     fill_in 'message_content', with: 'Lorem Ipsum'
     select2(first_receiver.first_name, from: 'message_recipient_ids')
+    select2(second_receiver.first_name, from: 'message_recipient_ids')
 
     click_on 'Send'
   end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -6,4 +6,23 @@ module FeatureHelpers
     page.execute_script(script)
     find(".select2-results__option", text: value).click
   end
+
+  def self.included(base)
+    base.before(:each) { Warden.test_mode! }
+    base.after(:each) { Warden.test_reset! }
+  end
+
+  def sign_in(resource)
+    login_as(resource, scope: warden_scope(resource))
+  end
+
+  def sign_out(resource)
+    logout(warden_scope(resource))
+  end
+
+  private
+
+  def warden_scope(resource)
+    resource.class.name.underscore.to_sym
+  end
 end
