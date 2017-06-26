4ee43add0417ff4532a93a232092646b08be73d3
zulkarnay <bedelbaev@gmail.com>
2017-06-26 17:32:19 +0600

    Filter by sender

--- app/controllers/messages/inbox_controller.rb
+++ app/controllers/messages/inbox_controller.rb
@@ -4,10 +4,15 @@ class Messages::InboxController < ApplicationController
   private
 
   def set_messages
-    @messages = current_user.received_messages
+    messages = current_user.received_messages
                       .includes(:sender)
                       .includes(:user_messages)
                       .paginate(page: params[:page])
                       .order(created_at: :desc)
+    if params[:sender]
+      messages = messages.where(sender_id: params[:sender])
+    end
+
+    @messages = messages
   end
 end
